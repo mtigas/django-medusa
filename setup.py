298cf53e8fd6bb29d65624dcc218c86e4e594435
Mike Tigas <mike@tig.as>
2014-10-16 22:52:28 -0400

    0.3.0, with fixes for python3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = [
     'django',
 ]
 
-version = __import__('django_medusa').get_version()
+version = "0.3.0"
 
 setup(name='django-medusa',
     version=version,
@@ -12,7 +12,7 @@ setup(name='django-medusa',
     author='Mike Tigas', # update this as needed
     author_email='mike@tig.as', # update this as needed
     url='https://github.com/mtigas/django-medusa/',
-    download_url='https://github.com/mtigas/django-medusa/downloads',
+    download_url='https://github.com/mtigas/django-medusa/releases/tag/v0.3.0',
     packages=find_packages(),
     install_requires=install_requires,
     license='MIT',
